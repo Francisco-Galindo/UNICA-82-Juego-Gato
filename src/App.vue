9a7b27ad19fc1765a835dda8fd2e70dce86e6328
Francisco-Galindo <paqui10718@gmail.com>
2023-04-13 03:46:23 -0600

    feat: Creando tablero, se pueden hacer movimientos

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,71 @@
 <script setup>
-import HelloWorld from './components/HelloWorld.vue'
+import Celda from './components/Celda.vue'
+import { ref, reactive } from 'vue'
+
+const turno = ref('x');
+
+const tablero = reactive([]);
+const gridTemplateCols = ref('');
+
+const tam = 3;
+for (let i = 0; i < tam; i++) {
+  tablero.push(Array(tam).fill(''));
+}
+
+for (let i = 0; i < tam; i++) {
+  gridTemplateCols.value += 'auto ';
+}
+
+function llenarCelda(i, j) {
+  if (tablero[i][j] !== '') {
+    return;
+  }
+
+  tablero[i][j] = turno.value;
+  if (turno.value === 'x') {
+    turno.value = 'o';
+  } else {
+    turno.value = 'x';
+  }
+
+}
 </script>
 
 <template>
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
+
+  <h2 v-if="turno === 'x'">Es turno de las X</h2>
+  <h2 v-else>Es turno de las Y</h2>
+
+  <div class="grid-container">
+
+    <template v-for="(row, i) in tablero">
+
+      <div v-for="(cell, j) in tablero[i]" class="cell-container">
+
+        <Celda
+          @click="llenarCelda(i, j)"
+          :i="i"
+          :j="j"
+          :estado="tablero[i][j]"
+          :turno="turno"/>
+      </div>
+
+    </template>
   </div>
-  <HelloWorld msg="Vite + Vue" />
+
 </template>
 
 <style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
+
+.grid-container {
+  display: grid;
+  grid-template-columns: v-bind(gridTemplateCols);
+  border: 1px solid;
 }
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
+
+.cell-container {
+  width: 5em;
+  height: 5em;
 }
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
+
 </style>
---
+++ src/components/Celda.vue
@@ -0,0 +1,58 @@
+<script setup>
+import { ref } from 'vue'
+
+defineProps({
+  estado: String,
+  turno: String,
+  i: Number,
+  j: Number,
+})
+
+</script>
+
+<template>
+
+  <div class="filled-cell x-filled" v-if="estado === 'x'">
+  </div>
+  <div class="filled-cell o-filled" v-else-if="estado === 'o'">
+  </div>
+  <div class="empty-cell x-outline" v-else-if="turno === 'x'">
+  </div>
+  <div class="empty-cell o-outline" v-else-if="turno === 'o'">
+  </div>
+
+</template>
+
+<style scoped>
+
+.empty-cell, .filled-cell {
+  height: 100%;
+  width: 100%;
+  background-repeat: no-repeat;
+  background-size: fill;
+  background-position: center;
+  border: 1px solid;
+}
+
+.empty-cell:hover {
+  box-shadow: 4px 7px 20px 1px rgba(0, 0, 0, 0.75);
+  cursor: pointer;
+}
+
+.x-outline:hover {
+  background-image: url('/src/assets/images/icon-x-outline.svg');
+}
+
+.o-outline:hover {
+  background-image: url('/src/assets/images/icon-o-outline.svg');
+}
+
+.x-filled {
+  background-image: url('/src/assets/images/icon-x.svg');
+}
+
+.o-filled {
+  background-image: url('/src/assets/images/icon-o.svg');
+}
+
+</style>
